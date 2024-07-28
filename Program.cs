2222a54554c9c63e64c4e425b7395e3add1c8548
Maury Elvis <mauryelvis@hotmail.com>
2024-07-28 16:05:43 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone= new Iphone("61998165983", "Iphone 15", "1234567891012", 128);
+Console.WriteLine("iPHONE");
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Nokia nokia= new Nokia("61998165983", "Nokia 2", "1234567891013", 128);
+Console.WriteLine("NOKIA");
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Duolingo");
